aaae021b14585b9e8f915fd66e125a6200393ca9
YagoConstantino <yagoa.const@outlook.com.br>
2025-04-02 11:18:33 -0300

    Teste yago

---
+++ TesteYago/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_TesteYago_FILE C:/Users/yagoa/Desktop/APS/TesteYago/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/TesteYago.exe)
+set(__QT_DEPLOY_TARGET_TesteYago_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_TesteYago_RUNTIME_DLLS C:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
