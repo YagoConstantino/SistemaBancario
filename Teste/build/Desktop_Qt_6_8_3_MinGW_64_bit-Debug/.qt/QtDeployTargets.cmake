3be7f0836255480da8df9f4c74d7afa63ff4518a
YagoConstantino <yagoa.const@outlook.com.br>
2025-04-01 23:00:38 -0300

    Teste de QT

---
+++ Teste/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Teste_FILE C:/Users/yagoa/Desktop/Teste/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/Teste.exe)
+set(__QT_DEPLOY_TARGET_Teste_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Teste_RUNTIME_DLLS C:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
