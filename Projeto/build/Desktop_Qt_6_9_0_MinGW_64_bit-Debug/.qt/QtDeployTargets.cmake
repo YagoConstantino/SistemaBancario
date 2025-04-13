1464e48057680b082deee390ff2b8519129e9a53
YagoConstantino <yagoa.const@outlook.com.br>
2025-04-13 19:04:30 -0300

    Apaguei build para testar

--- Projeto/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Projeto_FILE C:/Users/gabri/OneDrive/Documentos/Tudo/Back end/SistemaBancario/Projeto/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/Projeto.exe)
-set(__QT_DEPLOY_TARGET_Projeto_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Projeto_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
